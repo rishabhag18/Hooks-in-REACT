8e519afc9605655060e01f989a635c079359885e
rishabhag18 <18r.agarwal@gmail.com>
2024-08-10 12:52:57 +0530

    decreament button added

--- useState/src/components/Counter.jsx
+++ useState/src/components/Counter.jsx
@@ -6,7 +6,8 @@ const Counter=()=>{
         <div className={styles.container}>
             <h1>{`Hello Everyone. This is made using useState() Hooks.`}</h1>
             <p className={styles.countVariable}>{count}</p>
-            <button onClick={()=>setCount(count+1)} className={styles.increamentButton}>Increament</button>
+            <button onClick={()=>setCount(count+1)} className={styles.Button}>Increament</button>
+            <button className={styles.Button} onClick={()=>setCount(count&&count-1)}>Decreament</button>
         </div>
     )
 }
